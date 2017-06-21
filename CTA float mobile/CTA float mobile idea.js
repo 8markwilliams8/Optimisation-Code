5037ea7e60b5064bb8c583295c5f88f711e40631
Mark Williams <m@rkwilliams.co.uk>
2017-06-21 10:38:55 +0100

    21/06/17 - start exclusive test code

--- CTA float mobile/CTA float mobile idea.js
+++ CTA float mobile/CTA float mobile idea.js
@@ -1,3 +1,49 @@
+/////////////Working Idea////////////////////
+
+var utils = window['optimizely'].get('utils');
+utils.waitForElement('body').then(function(){
+
+var nav = $('.addSection__form'),
+  stopPosition = $('.stop-here').offset().top,
+  windowHeight = $( window ).height(),
+  stopPosition2 = stopPosition - windowHeight;
+
+
+
+$(document).scroll(function(){
+
+  var scrollPosition = $(window).scrollTop();
+
+  if (scrollPosition > stopPosition2){
+    nav.addClass('stickyBot');
+    if (scrollPosition > stopPosition2) {
+        nav.css('bottom', stopPosition2 - scrollPosition);
+    } else {
+        nav.css('bottom', 0);
+    }
+  }
+  else {
+    nav.removeClass('stickBot');
+  }
+});
+
+});
+
+//////////////Idea//////////////////////////
+
+
+var utils = window['optimizely'].get('utils');
+utils.waitForElement('body').then(function(){
+var targetOffset = $(".stop-here").offset().top;
+
+var $w = $(window).scroll(function(){
+    if ( $w.scrollTop() > targetOffset ) {
+        $('.addSection__form').css({"background-color":"#e10a0a"});
+    } else {
+      // ...
+    }
+});
+});
 
 
 ////////////////Idea/////////////////
